c1225265482e4f15f61e22a58c3d2f06240b390c
VenkatPagadala <venky.pagadala42@gmail.com>
2018-03-05 22:45:21 +0530

    by me

---
+++ Pvr/src/vv/man.java
@@ -0,0 +1,27 @@
+package vv;
+
+
+import java.util.Scanner;
+
+public class man {
+static int i;
+
+
+	public static void main(String[] args, Scanner in) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Enter two integers to calculate their sum");
+		
+		i = in.nextInt();
+		if(i>3){
+			System.out.println("100");
+		}
+		else{
+			System.out.println("200");
+			
+		}
+		System.out.println(i);
+	}
+
+}
