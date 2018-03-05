c7548dffcb0edf64d92045703308aab999e2ceb9
VenkatPagadala <venky.pagadala42@gmail.com>
2018-03-05 19:29:21 +0530

    by me

---
+++ Welcome/src/Hello/numbers.java
@@ -0,0 +1,15 @@
+package Hello;
+
+public class numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hi");
+		for (int i=1;i<=10;i++);
+		
+		System.out.println("welcome to my world");
+		
+		
+	}
+
+}
