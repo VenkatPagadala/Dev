e58bf6ff2858904b89dc4548341bc94aa1c4ca11
VenkatPagadala <venky.pagadala42@gmail.com>
2018-03-05 22:17:02 +0530

    by pvr

---
+++ Pvr/src/vv/ss.java
@@ -0,0 +1,20 @@
+package vv;
+
+public class ss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello ");
+		for (int i=1;i<=10;i++);
+		int i = 1;
+		if(i>=5){  
+			        System.out.print("Age is greater than 18");  
+			    }  
+		else{
+		System.out.println(i);
+			}  
+	}
+		
+
+	}
+
