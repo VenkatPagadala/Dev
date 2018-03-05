d09716a7546c25ce7b2b4ece028aa4220739155c
VenkatPagadala <venky.pagadala42@gmail.com>
2018-03-05 18:49:22 +0530

    commit by me

---
+++ Welcome/src/Hello/add.java
@@ -0,0 +1,25 @@
+package Hello;
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  {
+		      int x, y, z;
+		 
+		      System.out.println("Enter two integers to calculate their sum");
+		      @SuppressWarnings("resource")
+			Scanner in = new Scanner(System.in);
+		 
+		      x = in.nextInt();
+		      y = in.nextInt();
+		      z = x + y;
+		 
+		      System.out.println("Sum of the integers = " + z);
+		   }
+		}
+
+	}
+
+
