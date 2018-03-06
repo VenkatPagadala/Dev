1becfdbce6d264a0fb9a1a7b287eecb504a61986
VenkatPagadala <venky.pagadala42@gmail.com>
2018-03-06 14:37:01 +0530

    by me

---
+++ ramana/src/ramana/a1.java
@@ -0,0 +1,15 @@
+package ramana;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 10;
+
+    	if (number > 0) {
+    		System.out.println("Number is positive.");
+    	}
+    	System.out.println("This statement is always executed.");
+
+	}
+}
